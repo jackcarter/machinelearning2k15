aa9eb38865b8c171530c6b2d75057bc198e081cd
Mike Joyce <michael.b.joyce@gmail.com>
2015-10-21 22:20:19 -0500

    Store values to impute.

--- HW 4 - MJ.r
+++ HW 4 - MJ.r
@@ -20,6 +20,8 @@ prop_na <- function(x){return(sum(is.na(x))/length(x))}
 ##RETURNS PROPORTION OF VECTOR WITH THE MOST COMMON VALUE
 prop_most_common_val <- function(x){return(as.numeric(tail(sort(prop.table(table(x))),1)))}
 
+
+
 ##DROPS COLUMNS
 drops <- c()
 for(i in 2:ncol(features)){
@@ -45,14 +47,22 @@ replace_na_mean(c(NA, NA, 1, 2, 3))
 
 ###Missing values: numeric
 ###Missing values: categorical
+###Store the values we'll impute on the training data so we can do that on the test data too
+values_to_impute <- data.frame(var = names(features_sans_na),
+                               val = 0)
+
 for(i in 2:ncol(features_sans_na)){
   
   if(is.numeric(features_sans_na[[i]])) {
     features_sans_na[[i]] <- replace_na_mean(features_sans_na[[i]])
     print(paste0("replacing NAs in ", names(features_sans_na)[i], " with mean"))
+    values_to_impute[i,2] <- as.character(mean(features_sans_na[[i]]))
+    
   } else {
     features_sans_na[[i]] = factor(features_sans_na[[i]], levels=c(levels(features_sans_na[[i]]), "UNKNOWN"))
     features_sans_na[[i]][is.na(features_sans_na[[i]])] = "UNKNOWN"
+    values_to_impute[i,2] <- as.character("UNKNOWN")
+    
   }
 }
 
@@ -69,3 +79,14 @@ for (i in 1:ncol(df_factor)){
 }
 
 arrange(df_factor_levels, levels)
+
+
+
+
+##RUN SINGLE-VARIABLE CLASSIFIERS?
+##AND DROP COLUMNS
+
+
+##HOW DO WE CLEAN THE TEST DATA?
+##WE HAVE TO SAVE THE VALUES TO IMPUTE...
+## WE ALSO HAVE TO 
