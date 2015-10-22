367b02c2e153da99eaa43e4eef37ca16ca2827d6
Mike Joyce <michael.b.joyce@gmail.com>
2015-10-21 22:53:42 -0500

    Split train and test; try to impute training means into the test

--- HW 4 - MJ.r
+++ HW 4 - MJ.r
@@ -11,6 +11,11 @@ labels <- read.table("//Users/michaeljoyce/Downloads/DATA___KDDCup2009_Customer_
 
 
 
+###split
+train.ind <- sample(seq_len(nrow(features)), size = round(nrow(features)/2,0))
+train <- features[train.ind, ]
+test <- features[-train.ind, ]
+
 ###Figure out what needs cleaning
 ###Missing values (total)
 
@@ -23,18 +28,18 @@ prop_most_common_val <- function(x){return(as.numeric(tail(sort(prop.table(table
 
 
 ##DROPS COLUMNS
-drops <- c()
-for(i in 2:ncol(features)){
-  if(prop_na(features[[i]]) > .6) {
-    print(paste0("dropping ", names(features)[i], " because 60% of values are NA"))
+drops <- c(1)
+for(i in 2:ncol(train)){
+  if(prop_na(train[[i]]) > .6) {
+    print(paste0("dropping ", names(train)[i], " because 60% of values are NA"))
     drops <- c(drops, i)
     
-  } else if(prop_most_common_val(features[[i]]) > .99) {
-    print(paste0("dropping ", names(features)[i], " because 99% of values are same"))
+  } else if(prop_most_common_val(train[[i]]) > .99) {
+    print(paste0("dropping ", names(train)[i], " because 99% of values are same"))
     drops <- c(drops, i)
   }
 }
-features_sans_na <- features[-drops]
+train_sans_na <- train[-drops]
 
 
 ##replace NA with mean
@@ -48,19 +53,19 @@ replace_na_mean(c(NA, NA, 1, 2, 3))
 ###Missing values: numeric
 ###Missing values: categorical
 ###Store the values we'll impute on the training data so we can do that on the test data too
-values_to_impute <- data.frame(var = names(features_sans_na),
+values_to_impute <- data.frame(var = names(train_sans_na),
                                val = 0)
 
-for(i in 2:ncol(features_sans_na)){
+for(i in 1:ncol(train_sans_na)){
   
-  if(is.numeric(features_sans_na[[i]])) {
-    features_sans_na[[i]] <- replace_na_mean(features_sans_na[[i]])
-    print(paste0("replacing NAs in ", names(features_sans_na)[i], " with mean"))
-    values_to_impute[i,2] <- as.character(mean(features_sans_na[[i]]))
+  if(is.numeric(train_sans_na[[i]])) {
+    train_sans_na[[i]] <- replace_na_mean(train_sans_na[[i]])
+    print(paste0("replacing NAs in ", names(train_sans_na)[i], " with mean ", mean(train_sans_na[[i]])))
+    values_to_impute[i,2] <- as.character(mean(train_sans_na[[i]]))
     
   } else {
-    features_sans_na[[i]] = factor(features_sans_na[[i]], levels=c(levels(features_sans_na[[i]]), "UNKNOWN"))
-    features_sans_na[[i]][is.na(features_sans_na[[i]])] = "UNKNOWN"
+    train_sans_na[[i]] = factor(train_sans_na[[i]], levels=c(levels(train_sans_na[[i]]), "UNKNOWN"))
+    train_sans_na[[i]][is.na(train_sans_na[[i]])] = "UNKNOWN"
     values_to_impute[i,2] <- as.character("UNKNOWN")
     
   }
@@ -68,7 +73,7 @@ for(i in 2:ncol(features_sans_na)){
 
 ###Outliers? Numeric
 ###Many values per categorical feature
-df <- features_sans_na
+df <- train_sans_na
 
 factors <- sapply(df, is.factor)
 df_factor <- df[,factors]
@@ -81,12 +86,25 @@ for (i in 1:ncol(df_factor)){
 arrange(df_factor_levels, levels)
 
 
-
-
+#####~~~~****TEST DATA PROCESSING BEGIN****~~~~~#####
+replace_na_impute=function(x, val){
+  x[is.na(x)]=val
+  return(x)
+} 
+test_sans_na <- test[-drops]
+for (i in 1:ncol(test_sans_na)) {
+  if (is.numeric(test_sans_na[[i]])) {
+    test_sans_na[[i]] <- replace_na_impute(test_sans_na[[i]],as.numeric(values_to_impute[i,2]))
+    print(paste0("replacing NAs in ", names(test_sans_na)[i], " with ", values_to_impute[i,2]))
+  } else {
+    test_sans_na[[i]] <- replace_na_impute(test_sans_na[[i]],as.factor(values_to_impute[i,2]))
+    print(paste0("replacing NAs in ", names(test_sans_na)[i], " with ", values_to_impute[i,2]))
+  }
+}
 ##RUN SINGLE-VARIABLE CLASSIFIERS?
 ##AND DROP COLUMNS
-
-
+for (i in 1:ncol(test_sans_na)) {
+ print(paste0(prop_na(test_sans_na[[i]]),names(test_sans_na)[i]))
+}
+prop_na(test$Var229)
 ##HOW DO WE CLEAN THE TEST DATA?
-##WE HAVE TO SAVE THE VALUES TO IMPUTE...
-## WE ALSO HAVE TO 
