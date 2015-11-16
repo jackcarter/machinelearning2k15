229084c572703fac5f0235815784ea23059cbc66
Tom Sherman <tom@tomsherman.com>
2015-11-16 08:53:26 -0600

    gbm stuff

--- Tanzanian Pumps/Models_gbm.R
+++ Tanzanian Pumps/Models_gbm.R
@@ -9,67 +9,10 @@ source("GetData.R")
 library(gbm)
 
 
-##fit and predict
-fboost1 <- gbm(is_f ~ .,
-              data=df_smtrain_gbm1,
-              distribution="bernoulli",
-              n.trees=1000,
-              interaction.depth=4,
-              shrinkage=.01)
-pred_is_f <- predict(fboost, newdata=df_test_gbm1, n.trees=1000, type="response")
-
-##fit and predict
-fboost2 <- gbm(is_fnr ~ .,
-               data=df_smtrain_gbm2,
-               distribution="bernoulli",
-               n.trees=1000,
-               interaction.depth=4,
-               shrinkage=.01)
-pred_is_fnr <- predict(fboost2, newdata=df_test_gbm2, n.trees=1000, type="response")
-
-##fit and predict
-fboost3 <- gbm(is_nf ~ .,
-               data=df_smtrain_gbm3,
-               distribution="bernoulli",
-               n.trees=1000,
-               interaction.depth=4,
-               shrinkage=.01)
-pred_is_nf <- predict(fboost3, newdata=df_test_gbm3, n.trees=1000, type="response")
-
-gbm_pred <- c()
-for (i in 1:length(pred_is_f)) {
-  f <- pred_is_f[i]
-  fnr <- pred_is_fnr[i]
-  nf <- pred_is_nf[i]
-  
-  scores <- c(f, fnr, nf)
-  max_score <- max(scores)
-  winner <- ""
-  
-  if (max_score == f) {
-    winner <- "functional"
-  } else if (max_score == fnr) {
-    winner <- "functional needs repair"
-  } else if (max_score == nf) {
-    winner <- "non functional"
-  } else {
-    winner <- "uhhhh" 
-  }
-  
-  gbm_pred <- c(gbm_pred, winner)
-}
-
-
-misclass_mod_gbm <- sum(gbm_pred != df_test[, "status_group"]) / nrow(df_test)
-cat("newdata misclassification rate for gbm: ", misclass_mod_gbm)
-
-
-
-
 ##settings for boosting
-idv = c(2, 4)
-ntv = c(1000, 5000)
-shv = c(.1, .01, .001)
+idv = c(6, 4)
+ntv = c(8000, 4000)
+shv = c(.1)
 setboost = expand.grid(idv,ntv,shv)
 colnames(setboost) = c("tdepth","ntree","shrink")
 
@@ -80,7 +23,7 @@ for(i in 1:nrow(setboost)) {
   
     ##fit and predict
   fboost1 <- gbm(is_f ~ .,
-                 data=df_smtrain_gbm1,
+                 data=df_train_gbm1,
                  distribution="bernoulli",
                  interaction.depth=setboost[i,1],
                  n.trees=setboost[i,2],
@@ -89,7 +32,7 @@ for(i in 1:nrow(setboost)) {
   
   ##fit and predict
   fboost2 <- gbm(is_fnr ~ .,
-                 data=df_smtrain_gbm2,
+                 data=df_train_gbm2,
                  distribution="bernoulli",
                  interaction.depth=setboost[i,1],
                  n.trees=setboost[i,2],
@@ -98,7 +41,7 @@ for(i in 1:nrow(setboost)) {
   
   ##fit and predict
   fboost3 <- gbm(is_nf ~ .,
-                 data=df_smtrain_gbm3,
+                 data=df_train_gbm3,
                  distribution="bernoulli",
                  interaction.depth=setboost[i,1],
                  n.trees=setboost[i,2],
@@ -132,3 +75,69 @@ for(i in 1:nrow(setboost)) {
   cat("newdata misclassification rate for gbm: ", misclass_mod_gbm, "\n")
 }
 
+
+
+
+
+
+
+
+
+
+########### ignore below
+
+# 
+# 
+# ##fit and predict
+# fboost1 <- gbm(is_f ~ .,
+#                data=df_smtrain_gbm1,
+#                distribution="bernoulli",
+#                n.trees=1000,
+#                interaction.depth=4,
+#                shrinkage=.01)
+# pred_is_f <- predict(fboost, newdata=df_test_gbm1, n.trees=1000, type="response")
+# 
+# ##fit and predict
+# fboost2 <- gbm(is_fnr ~ .,
+#                data=df_smtrain_gbm2,
+#                distribution="bernoulli",
+#                n.trees=1000,
+#                interaction.depth=4,
+#                shrinkage=.01)
+# pred_is_fnr <- predict(fboost2, newdata=df_test_gbm2, n.trees=1000, type="response")
+# 
+# ##fit and predict
+# fboost3 <- gbm(is_nf ~ .,
+#                data=df_smtrain_gbm3,
+#                distribution="bernoulli",
+#                n.trees=1000,
+#                interaction.depth=4,
+#                shrinkage=.01)
+# pred_is_nf <- predict(fboost3, newdata=df_test_gbm3, n.trees=1000, type="response")
+# 
+# gbm_pred <- c()
+# for (i in 1:length(pred_is_f)) {
+#   f <- pred_is_f[i]
+#   fnr <- pred_is_fnr[i]
+#   nf <- pred_is_nf[i]
+#   
+#   scores <- c(f, fnr, nf)
+#   max_score <- max(scores)
+#   winner <- ""
+#   
+#   if (max_score == f) {
+#     winner <- "functional"
+#   } else if (max_score == fnr) {
+#     winner <- "functional needs repair"
+#   } else if (max_score == nf) {
+#     winner <- "non functional"
+#   } else {
+#     winner <- "uhhhh" 
+#   }
+#   
+#   gbm_pred <- c(gbm_pred, winner)
+# }
+# 
+# 
+# misclass_mod_gbm <- sum(gbm_pred != df_test[, "status_group"]) / nrow(df_test)
+# cat("newdata misclassification rate for gbm: ", misclass_mod_gbm)
